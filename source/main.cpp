d73102b3fba736ed1784d1f1c763dc66b04ad1f6
Ethan Heimer <crooked@Ethans-MacBook-Pro.local>
2026-06-15 23:01:53 -0700

    Cleaned up main

--- source/main.cpp
+++ source/main.cpp
@@ -10,30 +10,146 @@
 #define ctrl(x) ((x) & 0x1f)
 
 using namespace std;
-using Node = shared_ptr<DoublyIndexedLinkedList<GapBuffer>::Node>;
+using Lines = DoublyIndexedLinkedList<GapBuffer>&;
+using Line = shared_ptr<DoublyIndexedLinkedList<GapBuffer>::Node>;
 
-void Save(DoublyIndexedLinkedList<GapBuffer>& lines, const string& fileName){
-    ofstream saveFile{fileName};
-    if(saveFile.is_open()){
-        for(auto node : lines){
-            saveFile << ((string)*node).c_str() << endl;
-        }
-        saveFile.close();
-    }
-}
+void InitScreen();
+void KillScreen();
+
+void LoadFile(string filenName, Lines& lines, Line& currentLine);
+
+int GetInput();
+
+void GotoNextLine(Line& currentLine);
+void GotoPreviousLine(Line& currentLine);
+
+void MoveCursorLeft(Line& currentLine);
+void MoveCursorRight(Line& currentLine);
+
+bool IsCursorAtBeginningOfLine(Line& currentLine);
+
+void DeleteCharacter(Line& currentLine);
+void InsertCharacter(Line& currentLine, char input);
+
+void DeleteLine(Lines& lines, Line& currentLine);
+Line InsertLine(Lines& lines, Line& currentLine);
+
+void AppendTextToNextLine(Line& currentLine);
+
+void ExecuteCommands(const int& input, bool& quitToken, Lines& lines, 
+        Line& currentLine, const string& fileName);
+
+void UpdateUI(Line& currentLine, int& lineOffset, int& colOffset);
+void Save(Lines& lines, const string& fileName);
 
 int main(int argc, char** argv){
     if(argc < 2){
         return 1; 
     }
 
-    const string fileName{argv[1]};
+    DoublyIndexedLinkedList<GapBuffer> lines;
+    Line currentLine;
+    string fileName{argv[1]};
+    LoadFile(fileName, lines, currentLine);
+
+    InitScreen();
+
+    bool quit = false;
+    int lineOffset = 0;
+    int colOffset = 0;
+    while(!quit){
+        int input = GetInput();
+        UpdateUI(currentLine, lineOffset, colOffset);
+        ExecuteCommands(input, quit, lines, currentLine, fileName);
+    }
+
+    KillScreen();
+}
+
+void ExecuteCommands(const int& input, bool& quitToken, Lines& lines, Line& currentLine, const string& fileName){
+    if(input == ERR)
+        return;
+
+    if(input == ctrl('x'))
+        quitToken = true;
+    else if(input == ctrl('w'))
+        Save(lines, fileName);
+    else if(input == ctrl('X')){
+        quitToken = true; 
+        Save(lines, fileName);
+    }
+    else if(input == KEY_DOWN){
+        GotoNextLine(currentLine);
+    } 
+    else if(input == KEY_UP){
+        GotoPreviousLine(currentLine);
+    }
+    else if(input == KEY_LEFT){
+        MoveCursorLeft(currentLine);
+    }
+    else if(input == KEY_RIGHT){
+        MoveCursorRight(currentLine);
+    }
+    else if(input == KEY_BACKSPACE){
+        if(IsCursorAtBeginningOfLine(currentLine)){
+            DeleteLine(lines, currentLine);
+        }
+        else
+            DeleteCharacter(currentLine);
+    } 
+    else if(input == '\n' || input == '\r'){
+        InsertLine(lines, currentLine);
+        AppendTextToNextLine(currentLine);
+    }
+    else
+        InsertCharacter(currentLine, input);
+
+}
+
+void UpdateUI(Line& currentLine, int& lineOffset, int& colOffset){ 
+        const int lineColWidth = 3;
+        int currentLineNumber = currentLine->index;
+        int currentCursorCol = currentLine->data->GetGapIndex();
+        int row, col;
+
+        getmaxyx(stdscr, row, col);
+
+        erase();        
+        Line currentNode = currentLine;
+        while(currentNode && currentNode->index != lineOffset){
+            currentNode = currentNode->previous.lock();   
+        }
+
+        for(int i = 0; i < row && currentNode; i++){
+            printw(" %*d| %s \n", 
+                    lineColWidth,
+                    currentNode->index+1,
+                    currentNode->data->Substring(colOffset, colOffset + col - 6 - lineColWidth).c_str());
+
+            currentNode = currentNode->next;
+        }
+        move(currentLineNumber - lineOffset, currentCursorCol - colOffset + lineColWidth+3);
+        refresh();
+        
+        while(currentLineNumber - lineOffset >= row - 5)
+            lineOffset++;
+
+        while(currentLineNumber < lineOffset)
+            lineOffset--;
+
+        while(currentCursorCol - colOffset > col - 10){
+            colOffset++;
+        }
+
+        while(currentCursorCol < colOffset)
+            colOffset--;
+
+}
+
+void LoadFile(string fileName, Lines& lines, Line& currentLine){
     ifstream inputFile{fileName};
 
-    DoublyIndexedLinkedList<GapBuffer> lines;
-    Node currentLine;
     string line;
-
     if(inputFile.is_open()){
         while(getline(inputFile, line)){
             lines.Append(line, 5);
@@ -48,7 +164,9 @@ int main(int argc, char** argv){
         lines.Append("", 5);    
         currentLine = lines.head;
     }
+}
 
+void InitScreen(){
     setlocale(LC_ALL, "");
     initscr();
     noecho();
@@ -59,110 +177,81 @@ int main(int argc, char** argv){
     nonl();
     scrollok(stdscr, FALSE);
     idlok(stdscr, FALSE);
+}
 
-    bool quit = false;
-    int lineOffset = 0;
-    int colOffset = 0;
-
-    while(!quit){
-        const int lineColWidth = 3;
-        int row, col;
-        getmaxyx(stdscr, row, col);
-
-        erase();
-        
-        Node currentNode = currentLine;
-        while(currentNode && currentNode->index != lineOffset){
-            currentNode = currentNode->previous.lock();   
-        }
-
-        for(int i = 0; i < row && currentNode; i++){
-            printw(" %*d| %s \n", 
-                    lineColWidth,
-                    currentNode->index+1,
-                    currentNode->data->Substring(colOffset, colOffset + col - 6 - lineColWidth).c_str());
-
-            currentNode = currentNode->next;
-        }
-        
-        int currentLineNumber = currentLine->index;
-        int currentCursorCol = currentLine->data->GetGapIndex();
-
-        while(currentLineNumber - lineOffset >= row - 5)
-            lineOffset++;
-
-        if(currentLineNumber < lineOffset)
-            lineOffset--;
-
-        while(currentCursorCol - colOffset > col - 10){
-            colOffset++;
-        }
-
-        while(currentCursorCol < colOffset)
-            colOffset--;
-
-        move(currentLineNumber - lineOffset, currentCursorCol - colOffset + lineColWidth+3);
-        refresh();
-
-        int input = getch();
-
-        if(input == ERR)
-            continue;
-
-        else if(input == ctrl('x'))
-            quit = true;
-        else if(input == ctrl('w'))
-            Save(lines, fileName);
-        else if(input == ctrl('X')){
-           quit = true; 
-           Save(lines, fileName);
-        }
-        else if(input == KEY_DOWN){
-            if(currentLine->next)
-                currentLine = currentLine->next;
-        } 
-        else if(input == KEY_UP){
-            if(currentLine->previous.lock())
-                currentLine = currentLine->previous.lock();
-        }
-        else if(input == KEY_LEFT){
-            currentLine->data->MoveGapLeft();
-        }
-        else if(input == KEY_RIGHT){
-            currentLine->data->MoveGapRight();
-        }
-        else if(input == KEY_BACKSPACE){
-            bool isGapAtBeginning = currentLine->data->IsGapAtBeginning();
-            if(isGapAtBeginning){
-                Node previousLine = currentLine->previous.lock();
-                if(previousLine){
-                    string data = currentLine->data->ToString();
-                    previousLine->data->Insert(data);
-
-                    lines.Remove(currentLine);
-                    if(currentLine->previous.lock())
-                        currentLine = currentLine->previous.lock();
-                }
-            }
-            else
-                currentLine->data->Delete();
-        } 
-        else if(input == '\n' || input == '\r'){
-            lines.AppendAfter(currentLine, "", 5);
-            int gapIndex = currentLine->data->GetGapIndex();
-            int endIndex = currentLine->data->BufferSize();
-            string substring = currentLine->data->Substring(gapIndex, endIndex);
-            currentLine->data->DeleteBetween(gapIndex, endIndex);
-
-            if(currentLine->next){
-                currentLine = currentLine->next;
-                currentLine->data->Insert(substring);
-                currentLine->data->MoveGapTo(0);
-            }
-        }
-        else
-            currentLine->data->Insert(input);
-    }
-
+void KillScreen(){
     endwin();
+}
+
+void GotoNextLine(Line& currentLine){
+    if(currentLine->next)
+        currentLine = currentLine->next;
+}
+
+void GotoPreviousLine(Line& currentLine){
+    if(currentLine->previous.lock())
+        currentLine = currentLine->previous.lock();
+}
+
+void MoveCursorLeft(Line& currentLine){
+    currentLine->data->MoveGapLeft();
+}
+
+void MoveCursorRight(Line& currentLine){
+    currentLine->data->MoveGapRight();
+}
+
+bool IsCursorAtBeginningOfLine(Line& currentLine){
+    return currentLine->data->IsGapAtBeginning();
+}
+
+void InsertCharacter(Line& currentLine, char character){
+    currentLine->data->Insert(character);
+}
+
+void DeleteCharacter(Line& currentLine){
+    currentLine->data->Delete();
+}
+
+Line InsertLine(Lines& lines, Line& currentLine){
+    return lines.AppendAfter(currentLine, "", 5);
+}
+
+void DeleteLine(Lines& lines, Line& currentLine){
+    Line previousLine = currentLine->previous.lock();
+    if(previousLine){
+        string data = currentLine->data->ToString();
+        previousLine->data->Insert(data);
+
+        lines.Remove(currentLine);
+        if(currentLine->previous.lock())
+            currentLine = currentLine->previous.lock();
+    }
+}
+
+void AppendTextToNextLine(Line& currentLine){
+    int gapIndex = currentLine->data->GetGapIndex();
+    int endIndex = currentLine->data->BufferSize();
+    string substring = currentLine->data->Substring(gapIndex, endIndex);
+    currentLine->data->DeleteBetween(gapIndex, endIndex);
+
+    if(currentLine->next){
+        currentLine = currentLine->next;
+        currentLine->data->Insert(substring);
+        currentLine->data->MoveGapTo(0);
+    }
+}
+
+void Save(DoublyIndexedLinkedList<GapBuffer>& lines, const string& fileName){
+    ofstream saveFile{fileName};
+    if(saveFile.is_open()){
+        for(auto node : lines){
+            saveFile << ((string)*node).c_str() << endl;
+        }
+        saveFile.close();
+    }
+}
+
+int GetInput(){
+    return getch();
 }
